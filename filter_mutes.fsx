9d5705ccafef6d67482bfc8b63bc88537def5e7a
HKGx <19597269+HKGx@users.noreply.github.com>
2021-02-02 23:59:38 +0100

    Don't refilter mutes

--- filter_mutes.fsx
+++ filter_mutes.fsx
@@ -21,20 +21,51 @@ type Mutes = JsonProvider<SAMPLE>
 let mutes =
     Mutes.Load($"{__SOURCE_DIRECTORY__}/lista_mute.json")
 
+let lastValue = (mutes |> Array.last).JsonValue
+
+
+let mutable lastFilterMuteFlag = true
+
+[<Literal>]
+let LAST_FILTER_MUTE_PATH =
+    __SOURCE_DIRECTORY__ + "/last_filter_mute.json"
+
+if File.Exists(LAST_FILTER_MUTE_PATH) then
+    let value =
+        JsonValue.Load(File.OpenRead(LAST_FILTER_MUTE_PATH))
+
+    if lastValue = value then
+        lastFilterMuteFlag <- false
+    else
+        let last = mutes |> Array.last
+        File.WriteAllText(LAST_FILTER_MUTE_PATH, last.JsonValue.ToString())
+else
+    let last = mutes |> Array.last
+    File.WriteAllText(LAST_FILTER_MUTE_PATH, last.JsonValue.ToString())
+
+
+
+
 let onlyDigits (s: string) = s |> Seq.forall System.Char.IsDigit
 
 let onlyDigitsInUserId (m: Mutes.Root) = m.UserId |> onlyDigits
 
 let tuple a b = (a, b)
 
-let filtered = mutes |> Array.filter onlyDigitsInUserId
 
-filtered
-|> Array.map (fun m -> m.JsonValue.ToString())
-|> String.concat ","
-|> sprintf "[\n%s\n]"
-|> tuple "mutes.json"
-|> File.WriteAllText
+if lastFilterMuteFlag then
+    let filtered = mutes |> Array.filter onlyDigitsInUserId
 
-stopwatch.Stop()
-printfn $"Filtered out {mutes.Length - filtered.Length} mutes in {stopwatch.Elapsed.TotalMilliseconds}ms"
+    filtered
+    |> Array.map (fun m -> m.JsonValue.ToString())
+    |> String.concat ","
+    |> sprintf "[\n%s\n]"
+    |> tuple "mutes.json"
+    |> File.WriteAllText
+
+    stopwatch.Stop()
+    printfn $"Filtered out {mutes.Length - filtered.Length} mutes in {stopwatch.Elapsed.TotalMilliseconds}ms"
+else
+    stopwatch.Stop()
+
+    printfn $"Last mute hasn't changed. It took us {stopwatch.Elapsed.TotalMilliseconds}ms"
