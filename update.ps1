f3a9a1dd6531ceb82f7582163e4fe7a59d079c66
HKGx <19597269+HKGx@users.noreply.github.com>
2021-03-01 20:00:29 +0100

    Script for simpler updating of data

---
+++ update.ps1
@@ -0,0 +1,21 @@
+git pull
+Write-Information "Pulled from origin"
+if(-Not $args[0]) {
+    throw "You haven't provided the file to download"
+}
+Invoke-WebRequest $args[0] -OutFile "lista_mute.json"
+$date = Get-Item lista_mute.json
+| ForEach-Object { $_.CreationTime } 
+| ForEach-Object { $_.ToUniversalTime() }
+| ForEach-Object { $_.ToString() }
+
+$_ErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = "Stop"
+Get-Content "lista_mute.json" | ConvertFrom-Json  | ConvertTo-Json  | Out-Null
+$ErrorActionPreference = $_ErrorActionPreference
+
+Write-Information "Downloaded and validated the file"
+git add lista_mute.json
+$str = "Data, gathered at $date UTC"
+Write-Output $str
+git commit -m "`"$str`""
